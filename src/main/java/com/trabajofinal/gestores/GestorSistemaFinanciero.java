1d53899f672cb6d1410d6af9d18799ba6a5e9986
Diego Danelone <ddanelone@frsf.utn.edu.ar>
2023-12-03 20:06:28 -0300

    Terminada la lógica. Arreglé el temita ese de los descuentos semenstrales. Le agregué la barra de progreso a confirmar póliza

--- src/main/java/com/trabajofinal/gestores/GestorSistemaFinanciero.java
+++ src/main/java/com/trabajofinal/gestores/GestorSistemaFinanciero.java
@@ -1,34 +1,35 @@
 package com.trabajofinal.gestores;
 
 import com.trabajofinal.dto.PolizaDTO;
+import com.trabajofinal.models.TipoPago;
 
 public class GestorSistemaFinanciero {
-	
-	private static GestorSistemaFinanciero instance;
 
-	private GestorSistemaFinanciero() {
-		
-	}
-	
-	public static GestorSistemaFinanciero getInstance() {
-		if(instance == null) {
-			instance = new GestorSistemaFinanciero();
-		}
-		return instance;
-	}
-	
-	public void calcularPremioDerechosDescuentos(PolizaDTO poliza) {
+   private static GestorSistemaFinanciero instance;
 
-	      poliza.setDerechos_emision(1000.0);
-	      poliza.setPremio(5000.0);
-	      // poliza.setDescuentos(500.0);
-	      poliza.setDto_pago_semestral(100.0);
-	      poliza.setDto_antiguedad(50.90);
-	      poliza.setDto_mas_un_vehiculo(150.99);
-	      Double monto_a_pagar = poliza.getPremio() + poliza.getDerechos_emision() - poliza.getDto_antiguedad()
-	            - poliza.getDto_mas_un_vehiculo() - poliza.getDto_pago_semestral();
-	      poliza.setMonto_a_pagar(monto_a_pagar);
+   private GestorSistemaFinanciero() {
 
-}
+   }
+
+   public static GestorSistemaFinanciero getInstance() {
+      if (instance == null) {
+         instance = new GestorSistemaFinanciero();
+      }
+      return instance;
+   }
+
+   public void calcularPremioDerechosDescuentos(PolizaDTO poliza) {
+
+      poliza.setDerechos_emision(1000.0);
+      poliza.setPremio(5000.0);
+      Double descto_sem = (poliza.getTipoPago().equals(TipoPago.SEMESTRAL)) ? 100.0 : 0.0;
+      poliza.setDto_pago_semestral(descto_sem);
+      poliza.setDto_antiguedad(50.90);
+      poliza.setDto_mas_un_vehiculo(150.99);
+      Double monto_a_pagar = poliza.getPremio() + poliza.getDerechos_emision() - poliza.getDto_antiguedad()
+            - poliza.getDto_mas_un_vehiculo() - poliza.getDto_pago_semestral();
+      poliza.setMonto_a_pagar(monto_a_pagar);
+
+   }
 
 }
